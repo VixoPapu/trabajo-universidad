5536b8b452232879e4a1ab6e174e00983563fb1d
Vicente Ponce <vicente.ponce.018@gmail.com>
2025-10-14 14:35:14 -0300

    clases

---
+++ Trabajo2/models/cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+    def __init__ (self, telefono, direccion, run, nombre, apellido):
+        self.__telefono = telefono
+        self.__direccion = direccion
+        self.__run = run
+        self.__nombre = nombre
+        self.__apellido = apellido
+
